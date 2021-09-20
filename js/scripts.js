559cca8271bc8e11b8a13fccb39f599f6204cf84
job karani <job.nyambura@student.moringaschool.com>
2021-09-20 16:03:21 +0300

    adds images on the footer part

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 // business logic
-function
+function Size(type,price){
+    this.type=type;
+    this.price=price 
+}
 
-
+// user interface logic
 $(document).ready(function(){
+$("form#pizza").submit(function(e){
+    e.preventDefault();
 
+    let selectedType = $("select#type").val();
+})
 
 })
